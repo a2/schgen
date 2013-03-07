35d709ffec71db38bb0a4fc5b617929e48932884
Adam Obeng <github@binaryeagle.com>
2013-03-07 14:44:12 -0500

    Add function to query ADI API
    Signed-off-by: Adam Obeng <github@binaryeagle.com>

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
+from flask import (Flask, request, url_for, jsonify)
 import os
-from flask import (Flask, request, render_template, url_for, g, jsonify,
-                   redirect, abort, make_response)
+import json
+import urllib
+import urllib2
 
 app = Flask(__name__)
+
+def make_api_query(term, course):
+    base_url = 'http://data.adicu.com/courses?'
+    params = urllib.urlencode({
+        'api_token': app.DATA_ADICU_COM_API_KEY,
+            'term': term,
+            'course': course
+        }
+    )
+
+    url = base_url + params
+    print url
+
+    results = json.loads(urllib2.urlopen(url).read())
+    return results
+    
 
 @app.route('/')
 def hello():
     return render_template('index.html')
 
+
+@app.route('/courses.json')
+def courses():
+    results = make_api_query('spring2013', 'MATHV1202')
+    return jsonify(results)
+
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
+
+    app.DATA_ADICU_COM_API_KEY = os.environ.get('DATA_ADICU_COM_API_KEY')
+
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
