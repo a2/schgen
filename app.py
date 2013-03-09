62423f572f91c3e3410e8555660e137ea697f997
Alexsander Akers <a2@pandamonia.us>
2013-03-09 16:48:45 -0500

    Revert "Encapsulate search (-> search_json) and courses (-> courses_json) functions"
    This reverts commit 75bd6f187ebc5b1d0af51c3d81054f7ab3a8079a.

--- app.py
+++ app.py
@@ -30,131 +30,65 @@ def section_combinations(courses):
     return list(itertools.product(*sn))
 
 
-def search(term, course='', title='', description=''):
-    """Searches for courses during a given term based on course ID, title,
-    or description.
+@app.route('/')
+def hello():
+    return render_template('index.html')
 
-    Args:
-        term (str): The term to search in.
-
-    Kwargs:
-        course (str): The course ID to search for.
-        title (str): The title of the course to search for.
-        description (str): The description of the course to search for.
-
-    Returns:
-        dict. The course data:
-            {
-                'results': [
-                    {...},
-                    {...},
-                    ...
-                ]
-            }
-
-    """
-    if term and (course or title or description):
-        kwargs = {'term': term}
+@app.route('/search.json')
+def search():
+    term = request.args.get('term')
+    course = request.args.get('course')
+    title = request.args.get('title')
+    description = request.args.get('description')
+    
+    if term and (bool(course) + bool(title) + bool(description) == 1):
         if course:
-            kwargs['course'] = course
-        if title:
-            kwargs['title'] = title
-        if description:
-            kwargs['description'] = description
+            results = make_api_query(term=term, course=course)
+        elif title:
+            results = make_api_query(term=term, title=title)
+        elif description:
+            results = make_api_query(term=term, description=description)
 
-        results = make_api_query(**kwargs)
-        
         if results['status_code'] != 200:
             print 'API server returned errors'
-            return {}
+            abort(400)  # Bad request
 
-        return {'results':results['data']}
+        return jsonify({
+            'results': results['data']
+        })
     else:
         print 'Invalid parameters term=%s, course=%s, title=%s, description=%s' \
             % (term, course, title, description)
-        return {}
+        abort(400)  # Bad request
 
-
-def get_courses(term, courses):
-    """Retrieve course data for courses during a given term.
-
-    Args:
-        term (str): The term to search in.
-        courses (iterable): The courses to search for.
+@app.route('/courses.json')
+def courses():
+    term = request.args.get('term')
+    courses = request.args.get('courses')
     
-    Returns:
-        dict. The course data::
-            {
-                "combinations": [...],
-                "course_data": {
-                        "ENGL1010": {
-                                "ENGL1010C029": {...},
-                                "ENGL1010C028": {...},
-                                ...
-                        },
-                        "MATH1202": {
-                                "MATH1202V005": {...},
-                                "MATH1202V004", {...},
-                                ...
-                        },
-                        ...
-                }
-            }
-
-    """
-    if term and len(courses):
+    if term and courses:
+        courses = courses.split(',')
         results = {c: make_api_query(term=term, course=c) for c in courses if c}
 
         status_codes_OK = [v['status_code'] == 200 for v in results.values()]
 
         if not all(status_codes_OK):
             print 'API server returned errors'
-            return {}
+            abort(400)  # Bad request
 
         data = {}
         for key in results.keys():
             data[key] = {course['Course']: course for course in results[key]['data']}
 
-        return {
+        {k: results[k]['data'] for k in results.keys()}
+
+        return jsonify({
             'combinations': section_combinations(results),
             'course_data': data,
-        }
+        })
     else:
         print 'Invalid parameters term=%s, courses=%s' % (term, courses)
-        return {}
-
-
-@app.route('/')
-def hello():
-    return render_template('index.html')
-
-
-@app.route('/search.json')
-def search_json():
-    term = request.args.get('term')
-    course = request.args.get('course')
-    title = request.args.get('title')
-    description = request.args.get('description')
-    
-    results = search(term, course, title, description)
-
-    if len(results):
-        return jsonify(results)
-    else:
         abort(400)  # Bad request
-
-@app.route('/courses.json')
-def courses_json():
-    term = request.args.get('term')
-    courses = request.args.get('courses').split(',')
-    
-    results = get_courses(term, courses)
-
-    if len(results):
-        return jsonify(results)
-    else:
-        abort(400)  # Bad request
-
 
 if __name__ == '__main__':
     app.COLUMBIA_DAYS = "UMTWRFS"
