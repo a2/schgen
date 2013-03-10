7ad2691f81dc43e217f3e1569826ac8603e33f47
Alexsander Akers <a2@pandamonia.us>
2013-03-09 21:53:57 -0500

    Add first pass at GUI
    :D
    Signed-off-by: Alexsander Akers <a2@pandamonia.us>

---
+++ config.rb
@@ -0,0 +1,20 @@
+require 'zurb-foundation'
+
+# -----------------------------------------------
+# Paths
+# -----------------------------------------------
+
+http_path = "/"
+css_dir = "static/css"
+sass_dir = "static/sass"
+images_dir = "static/img"
+javascripts_dir = "static/js"
+
+# -----------------------------------------------
+# Output
+# -----------------------------------------------
+
+# output_style = :compressed
+preferred_syntax = :sass
+relative_assets = true
+line_comments = true
---
+++ static/js/app.js
@@ -0,0 +1,72 @@
+;(function ($, window, undefined) {
+	$(function() {
+		$(document).foundation();
+
+		$('#search-box').autocomplete({
+			source: function(request, response) {
+				$.ajax({
+					url: '/search.json',
+					dataType: 'json',
+					data: {
+						term: $("#term :checked").attr('name'),
+						query: request.term
+					},
+					success: function(data) {
+						console.log(data);
+						response(data.results);
+					}
+				});
+			},
+			focus: function(event, ui) {
+				// $('#search-box').val(ui.item.title);
+				return false;
+			},
+			select: function(event, ui) {
+				$.ajax({
+					url: '/sections.html',
+					dataType: 'html',
+					data: {
+						term: $("#term :checked").attr('name'),
+						course: ui.item.value
+					},
+					success: function(html) {
+						$('#accordion').append(html)
+					}
+				})
+			}
+		})
+		.data('ui-autocomplete')._renderItem = function(ul, item) {
+			return $('<li />')
+				.append(['<a><span class="title">', item.title, '</span><br /><span class="subtitle">', item.subtitle, '</span></a>'].join(''))
+				.appendTo(ul);
+		};
+
+		$('#search-button').click(function(event) {
+			$('#search-box').autocomplete('search', $('#search-box').val());
+			event.preventDefault();
+		});
+	});
+})(jQuery, this);
+
+/* 
+ * To Title Case 2.0.1 – http://individed.com/code/to-title-case/
+ * Copyright © 2008–2012 David Gouch. Licensed under the MIT License. 
+ */
+
+String.prototype.toTitleCase = function () {
+  var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|vs?\.?|via)$/i;
+
+  return this.replace(/([^\W_]+[^\s-]*) */g, function (match, p1, index, title) {
+    if (index > 0 && index + p1.length !== title.length &&
+      p1.search(smallWords) > -1 && title.charAt(index - 2) !== ":" && 
+      title.charAt(index - 1).search(/[^\s-]/) < 0) {
+      return match.toLowerCase();
+    }
+
+    if (p1.substr(1).search(/[A-Z]|\../) > -1) {
+      return match;
+    }
+
+    return match.charAt(0).toUpperCase() + match.substr(1);
+  });
+};
